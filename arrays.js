5c974d4e326c283077523f5708c4a7e56bbf8ea5
Rachel Feeken <rfeeken@gmail.com>
2017-11-03 14:35:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  const newArray =[element, ...array];
+  return newArray
+}
